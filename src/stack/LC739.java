745467edd8952793edb0e145028498d7d2548897
Sai Krishna Chowdary <kittusontineni@gmail.com>
2026-07-21 19:24:37 +0530

    ft: LeetCode 739 using Monotonic Decreasing Stack.

---
+++ src/stack/LC739.java
@@ -0,0 +1,23 @@
+package stack;
+
+import java.util.Stack;
+
+public class LC739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int n = temperatures.length;
+
+        int[] result = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int i = 0 ; i < n; i++){
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]){
+                int index = stack.pop();
+                result[index] = i - index;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
