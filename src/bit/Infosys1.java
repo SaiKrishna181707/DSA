86eff2b9eaaaa56ece18061e5eee7e29e1f527e1
Sai Krishna Chowdary <kittusontineni@gmail.com>
2026-07-27 20:22:48 +0530

    ft: unique among duplicates using XOR.

---
+++ src/bit/Infosys1.java
@@ -0,0 +1,22 @@
+package bit;
+
+import java.util.Scanner;
+
+public class Infosys1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans ^= sc.nextInt();
+        }
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
